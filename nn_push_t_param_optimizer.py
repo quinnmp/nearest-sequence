76701a604aa1a97769c4bc385491c739adf9f707
Quinn Pfeifer <quinnmpfeifer@gmail.com>
2024-11-14 15:39:54 -0800

    Messy Hyak commit

--- nn_push_t_param_optimizer.py
+++ nn_push_t_param_optimizer.py
@@ -62,7 +62,7 @@ def objective(obs1=1, obs2=1, obs3=1, obs4=1, obs5=1, candidate_num=100, lookbac
         env = gym.make(config['env'])
     np.random.seed(config['seed'])
 
-    agent = nn_agent.NNAgentEuclideanStandardized(config['data']['pkl'], method=nn_util.NN_METHOD.GMM, plot=False, candidates=int(candidate_num), lookback=int(lookback_num), decay=int(decay_num)/10, window=int(window_num), weights=np.array([obs1, obs2, obs3, obs4, obs5]), final_neighbors_ratio=final_neighbors_ratio, rot_indices=np.array([4]), cond_force_retrain=True)
+    agent = nn_agent.NNAgentEuclideanStandardized(config['data']['pkl'], method=nn_util.NN_METHOD.COND, plot=False, candidates=int(candidate_num), lookback=int(lookback_num), decay=int(decay_num)/10, window=int(window_num), weights=np.array([obs1, obs2, obs3, obs4, obs5]), final_neighbors_ratio=final_neighbors_ratio, rot_indices=np.array([4]), cond_force_retrain=True)
 
     episode_rewards = []
     for i in range(10):
